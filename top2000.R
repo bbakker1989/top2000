fd7a9c2c65309d2b70156c35efb3500b6d0293c5
Bjorn Bakker <31505702+bbakker1989@users.noreply.github.com>
2021-12-26 16:42:40 +0000

    Add files via upload

--- top2000.R
+++ top2000.R
@@ -44,6 +44,7 @@ p1 <- decade_count %>%
                            aes(label = decennium), hjust = -0.1, show.legend = FALSE) +
   expand_limits(x = c(1999, 2022)) +
   scale_x_continuous(breaks = seq(1999, 2021)) +
+  scale_color_manual(values = c('#b35806','#e08214','#fdb863','#fee0b6','darkgrey','#b2abd2','#8073ac','#542788')) +
   labs(x = "Editie", y = "Aantal nummers", colour = "Decennium",
        title = "Opkomst en 'ondergang' van verschillende decennia",
        subtitle = "Alleen datapunten van decennia met 5 nummers of meer worden getoond") +
@@ -77,6 +78,7 @@ p2 <- top_artiest_plot %>%
        subtitle = "Artiesten met de meeste nummers in 2021 worden getoond") +
   expand_limits(x = c(1999, 2021)) +
   scale_x_continuous(breaks = seq(1999, 2021)) +
+  scale_color_manual(values = c('#7f3b08','#b35806','#e08214','#fdb863','#fee0b6','darkgrey','#b2abd2','#8073ac','#542788','#2d004b')) +
   top2000_theme() +
   theme(legend.position = "bottom") +
   guides(col = guide_legend(nrow = 3))
@@ -85,12 +87,13 @@ p2
 # diversity in years across editions
 p3 <- top2000_tidy %>%
   na.omit() %>%
-  filter(editie %in% c(1999, 2005, 2010, 2015, 2021)) %>%
+  filter(editie %in% ceiling(seq(from = 1999, to = 2021, length.out = 6))) %>%
   ggplot(aes(x = jaar, group = editie, colour = factor(editie))) +
   geom_density(size = 1) +
   labs(x = "Jaar van uitgave", y = "Dichtheid", col = "Editie",
        title = "Diversiteit in tijdperken van nummers over vijf verschillende edities") +
   scale_x_continuous(breaks = seq(1920, 2030, 5)) +
+  scale_color_manual(values = c('darkgrey','#dadaeb','#bcbddc','#9e9ac8','#756bb1','#54278f')) +
   coord_cartesian(x = c(1950, 2021)) +
   top2000_theme() +
   theme(legend.position = "bottom")
@@ -104,17 +107,17 @@ top2000_mat <- top2000 %>%
   select(-artiest, -titel, -jaar, -decennium) %>%
   as.data.frame() %>%
   tibble::column_to_rownames("artiest_titel") %>%
-  mutate(across(.cols = 1:23, .fns = function(x) {ifelse(is.na(x), 2001, x)})) %>%
+  mutate(across(.cols = 1:23, .fns = function(x) {ifelse(is.na(x), 5000, x)})) %>%
   as.matrix()
 
 cluster_info <- hclust(dist(top2000_mat))
 artiest_levels <- rownames(top2000_mat)[cluster_info$order]
 
 p4 <- top2000_tidy %>%
-  mutate(artiest_titel = factor(artiest_titel, levels = artiest_levels)) %>%
+  mutate(artiest_titel = factor(artiest_titel, levels = rev(artiest_levels))) %>%
   ggplot(aes(x = editie, y = artiest_titel, fill = positie)) +
   geom_tile() +
-  scale_fill_viridis_c(na.value = "white", direction = -1) +
+  scale_fill_viridis_c(na.value = "white", direction = -1, breaks = c(1, 500, 1000, 1500, 2000)) +
   labs(x = "Editie", y = "Nummer", fill = "Positie",
        title = "Posities van alle nummers over de jaren heen",
        subtitle = "Nummers zonder een notering in een jaar worden getoond in wit") +
@@ -124,22 +127,52 @@ p4 <- top2000_tidy %>%
         axis.text.y = element_blank())
 p4
 
-ggsave(p, file = "plot.png", width = 6, height = 15)
+p5 <- top2000_tidy %>%
+  na.omit() %>%
+  filter(jaar >= 1950, 
+         editie %in% ceiling(seq(from = 1999, to = 2021, length.out = 6))) %>%
+  ggplot(aes(x = jaar, y = positie)) +
+  geom_point(shape = 1, alpha = 0.5, aes(col = decennium), show.legend = FALSE) +
+  geom_smooth(method = "lm", se = FALSE, size = 2) +
+  scale_y_reverse(breaks = c(2000, 1500, 1000, 500, 1), limits = c(2000, -75)) +
+  scale_x_continuous(breaks = seq(1950, 2020, 10)) +
+  ggpubr::stat_cor(label.y = 75) +
+  facet_wrap(~editie, scales = "free", shrink = TRUE) +
+  labs(x = "Jaar van uitgave", y = "Positie",
+       title = "Jaar van uitgave is niet (meer) voorspellend voor positie in de lijst",
+       subtitle = "Pearsons correlatiecoëfficient en p-waarden worden weergegeven") +
+  top2000_theme()
+p5
+
+p6 <- top2000_tidy %>%
+  na.omit() %>%
+  filter(jaar >= 1950) %>%
+  group_by(editie, decennium) %>%
+  summarize(n = n(), .groups = "drop") %>%
+  group_by(editie) %>%
+  mutate(f = 100*(n/sum(n))) %>%
+  ggplot(aes(x = editie, y = f, fill = decennium)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Editie", y = "Aantal nummers (%)", fill = "Decennium",
+       title = "Bijdrage van nummers uit verschillende decennia aan de lijst") +
+  scale_x_continuous(breaks = seq(1999, 2021, 1)) +
+  scale_y_continuous(breaks = seq(0, 100, 10)) +
+  scale_fill_manual(values = c('#b35806','#e08214','#fdb863','#fee0b6','darkgrey','#b2abd2','#8073ac','#542788')) +
+  top2000_theme() +
+  theme(legend.position = "bottom")
+p6
+
 ggsave(p1, file = "plot1.png", width = 6, height = 5)
 ggsave(p2, file = "plot2.png", width = 6, height = 5)
 ggsave(p3, file = "plot3.png", width = 6, height = 5)
 ggsave(p4, file = "plot4.png", width = 6, height = 5)
+ggsave(p5, file = "plot5.png", width = 6, height = 5)
+ggsave(p6, file = "plot6.png", width = 6, height = 5)
 
-top2000_tidy %>%
-  filter(jaar >= 1950) %>%
-  ggplot(aes(x = jaar, y = positie, group = editie, col = factor(editie))) +
-  #geom_point(shape = 1, col = "lightgrey") +
-  geom_hex() +
-  geom_smooth(method = "lm", se = FALSE) +
-  ggpubr::stat_cor(cor.coef.name = "rho") +
-  scale_y_reverse(breaks = c(2000, 1500, 1000, 500, 1)) +
-  #facet_wrap(~editie) +
-  top2000_theme()
+p <- list(p4, p2, p5, p1, p3, p6)
+p0 <- patchwork::wrap_plots(p, ncol = 3, nrow = 2)
+ggsave(p0, file = "plot0.png", width = 18, height = 12)
+
 
 cor_data <- top2000_tidy %>%
   na.omit() %>%
